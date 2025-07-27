84a97e19cdac346a6a21ce066dc7f563e22eaa40
Atmanand Gauns <sanketgauns8@gmail.com>
2025-07-27 16:44:55 +0530

    fix : hideEmptyColumns is undefined

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -32,7 +32,7 @@ const Column: React.FC<ColumnProps> = ({
 	columnData,
 	tasksForThisColumn,
 }) => {
-	if (plugin.settings.data.boardConfigs[activeBoardIndex].hideEmptyColumns && (tasksForThisColumn === undefined || tasksForThisColumn.length === 0)) {
+	if (plugin.settings.data.boardConfigs[activeBoardIndex]?.hideEmptyColumns && (tasksForThisColumn === undefined || tasksForThisColumn.length === 0)) {
 		return null; // Don't render the column if it has no tasks and empty columns are hidden
 	}
 	// Local tasks state, initially set from external tasks
