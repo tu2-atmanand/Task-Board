e45a799b2f3af61dc3c08ce059d9091fe28ca463
Atmanand Gauns <sanketgauns8@gmail.com>
2025-10-13 21:45:04 +0530

    add credits for the embeddedEditor code

--- src/services/MarkdownEditor.ts
+++ src/services/MarkdownEditor.ts
@@ -1,5 +1,10 @@
 // /src/services/MarkdownEditor.ts
 
+/**
+ * This complete EmbeddableMarkdownEditor component has been imported from Task Genius plugin.
+ * @see https://github.com/Quorafind/Obsidian-Task-Genius/blob/59a2d42f9fb95a5700d32975917e56372e5bdb7d/src/editor-extensions/core/markdown-editor.ts
+ */
+
 import {
 	App,
 	Editor,
