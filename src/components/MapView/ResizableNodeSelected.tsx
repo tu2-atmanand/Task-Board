740f8c74dbc1d6e4e85c3ac9718ee24cc36ab23e
Atmanand Gauns <sanketgauns8@gmail.com>
2025-10-25 21:58:13 +0530

    code refactoring

--- src/components/MapView/ResizableNodeSelected.tsx
+++ src/components/MapView/ResizableNodeSelected.tsx
@@ -3,7 +3,7 @@ import { Handle, Position, NodeResizer, NodeProps } from '@xyflow/react';
 import { nodeSize } from './MapView';
 import { NODE_SIZE_STORAGE_KEY } from 'src/interfaces/Constants';
 import type TaskBoard from 'main';
-import { mapViewNodeHandlePosition } from 'src/interfaces/Enums';
+import { mapViewNodeMapOrientation } from 'src/interfaces/Enums';
 
 interface dataProps extends React.ReactElement<unknown, string> {
 	props: { plugin: TaskBoard };
@@ -16,8 +16,7 @@ interface ResizableNodeSelectedProps {
 
 const ResizableNodeSelected: FC<NodeProps & ResizableNodeSelectedProps> = ({ id, data, selected, width, height }) => {
 	const mapViewSettings = data.label?.props?.plugin.settings.data.globalSettings.mapView;
-	const targetPosition = mapViewSettings.handlePosition;
-	const arrowForward = mapViewSettings.arrowForward;
+	const orientationHorizontal = mapViewSettings.mapOrientation === mapViewNodeMapOrientation.horizontal;
 	// console.log('Rendering ResizableNodeSelected for node:', id, { mapViewSettings, selected, width, height });
 
 	return (
@@ -35,7 +34,7 @@ const ResizableNodeSelected: FC<NodeProps & ResizableNodeSelectedProps> = ({ id,
 					try {
 						const sizeData: Record<string, nodeSize> = JSON.parse(localStorage.getItem(NODE_SIZE_STORAGE_KEY) || '{}');
 						sizeData[id] = {
-							width: params.width ?? 100,
+							width: params.width ?? 100
 							// height: params.height ?? 30 
 						};
 						localStorage.setItem(NODE_SIZE_STORAGE_KEY, JSON.stringify(sizeData));
@@ -44,56 +43,24 @@ const ResizableNodeSelected: FC<NodeProps & ResizableNodeSelectedProps> = ({ id,
 					}
 				}}
 			/>
-			{targetPosition === mapViewNodeHandlePosition.horizontal && (
-				<Handle autoFocus={false} type="target" position={Position.Left} />
+			{/* <Handle type="target" position={Position.Left} /> */}
+
+			{orientationHorizontal && (
+				<Handle type="target" position={Position.Left} />
 			)}
-			{targetPosition === mapViewNodeHandlePosition.vertical && (
+			{!orientationHorizontal && (
 				<Handle type="target" position={Position.Top} />
 			)}
 			<div className={`taskBoardMapViewContainerNodeResizerTaskItem${selected ? '-selected' : ''}`}>{data.label}</div >
-			{targetPosition === mapViewNodeHandlePosition.horizontal && (
-				<Handle autoFocus={false} type="source" position={Position.Right} />
-			)
-			}
-			{
-				targetPosition === mapViewNodeHandlePosition.vertical && (
-					<Handle type="source" position={Position.Bottom} />
-				)
-			}
 
-			{/* for both
-			{arrowForward ? (
-				<>
-					{targetPosition === mapViewNodeHandlePosition.both && (
-						<Handle type="target" position={Position.Top} />
-					)}
-					{targetPosition === mapViewNodeHandlePosition.both && (
-						<Handle type="target" position={Position.Left} />
-					)}
-					{targetPosition === mapViewNodeHandlePosition.both && (
-						<Handle type="source" position={Position.Bottom} />
-					)}
-					{targetPosition === mapViewNodeHandlePosition.both && (
-						<Handle type="source" position={Position.Right} />
-					)}
-				</>
-			) : (
-				<>
-					{targetPosition === mapViewNodeHandlePosition.both && (
-						<Handle type="source" position={Position.Top} />
-					)}
-					{targetPosition === mapViewNodeHandlePosition.both && (
-						<Handle type="source" position={Position.Left} />
-					)}
-					{targetPosition === mapViewNodeHandlePosition.both && (
-						<Handle type="target" position={Position.Bottom} />
-					)}
-					{targetPosition === mapViewNodeHandlePosition.both && (
-						<Handle type="target" position={Position.Right} />
-					)}
-				</>
-			)
-			} */}
+			{/* <Handle type="source" position={Position.Right} /> */}
+
+			{orientationHorizontal && (
+				<Handle type="source" position={Position.Right} />
+			)}
+			{!orientationHorizontal && (
+				<Handle type="source" position={Position.Bottom} />
+			)}
 		</>
 	);
 };
