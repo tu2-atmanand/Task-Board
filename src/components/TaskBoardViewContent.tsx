ebc83744db684b66f53f0aec3167bbd47e554494
Atmanand Gauns <sanketgauns8@gmail.com>
2025-11-22 18:49:11 +0530

    fix : the funnel icon looks very light

--- src/components/TaskBoardViewContent.tsx
+++ src/components/TaskBoardViewContent.tsx
@@ -20,7 +20,7 @@ import { ViewTaskFilterPopover } from "./BoardFilters/ViewTaskFilterPopover";
 import { boardFilterer } from "src/utils/algorithms/BoardFilterer";
 import { ViewTaskFilterModal } from 'src/components/BoardFilters';
 import { viewTypeNames } from "src/interfaces/Enums";
-import { funnelIconSVG, ScanVaultIcon } from "src/interfaces/Icons";
+import { ScanVaultIcon, funnelIcon } from "src/interfaces/Icons";
 
 const TaskBoardViewContent: React.FC<{ app: App; plugin: TaskBoard; boardConfigs: Board[] }> = ({ app, plugin, boardConfigs }) => {
 	const [boards, setBoards] = useState<Board[]>(boardConfigs);
@@ -43,9 +43,6 @@ const TaskBoardViewContent: React.FC<{ app: App; plugin: TaskBoard; boardConfigs
 	const [isMobileView, setIsMobileView] = useState(false);
 	const [showBoardSidebar, setShowBoardSidebar] = useState(false);
 	const [sidebarAnimating, setSidebarAnimating] = useState(false);
-
-	// Importing an SVG icon inside Obsidian application lucude icons library.
-	addIcon("funnelIcon", funnelIconSVG);
 
 	// plugin.registerEvent(
 	// 	plugin.app.workspace.on("resize", () => {
@@ -402,7 +399,7 @@ const TaskBoardViewContent: React.FC<{ app: App; plugin: TaskBoard; boardConfigs
 		});
 		sortMenu.addItem((item) => {
 			item.setTitle(t("open-board-filters-modal"));
-			item.setIcon("funnelIcon");
+			item.setIcon(funnelIcon);
 			item.onClick(async (event) => {
 				if (event instanceof MouseEvent) {
 					handleFilterButtonClick(event as unknown as React.MouseEvent<HTMLButtonElement, MouseEvent>);
--- src/interfaces/Icons.ts
+++ src/interfaces/Icons.ts
@@ -4,6 +4,8 @@ export const RefreshIcon = "rotate-cw";
 
 export const ScanVaultIcon = "lucide-scan-text";
 
+export const funnelIcon = "filter";
+
 export const taskBoardActionsIcon = "bot";
 
 export const trashIcon = "trash-2";
@@ -53,6 +55,3 @@ export const kofiSVGIcon = `
   </g>
 </svg>
 `;
-
-export const funnelIconSVG = `<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-<svg width="100" height="100" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" stroke-width="5" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-settings"><polygon points="8.5 8.46 55.5 8.46 55.38 15 36 34 36 55.54 26 50 26 34 8.5 15 8.5 8.46"/><line x1="8.5" y1="14.47" x2="55.5" y2="14.47"/></svg>`;
