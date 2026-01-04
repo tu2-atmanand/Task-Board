984990a40c6122c6abc014bb05a2595809b6a851
Atmanand Gauns <sanketgauns8@gmail.com>
2026-01-04 10:00:25 +0530

    add source credits for all the referenced code

--- src/modals/date_picker/index.ts
+++ src/modals/date_picker/index.ts
@@ -1,3 +1,17 @@
+// // src/modals/date_picker/index.ts
+
+/**
+ * Date Picker Modal
+ * This module has been inspired from the Task-Genius plugin by Boninall.
+ * @see https://github.com/taskgenius/taskgenius-plugin/
+ * @module modals/date_picker
+ * @requires src/modals/date_picker/DatePickerModal
+ * @requires src/modals/date_picker/DatePickerComponent
+ *
+ * Changes done to the original component : 
+ * - Added dateMark
+ */
+
 import { DatePickerComponent } from "./DatePickerComponent";
 import { DatePickerModal } from "./DatePickerModal";
 
