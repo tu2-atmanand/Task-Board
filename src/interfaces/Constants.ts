96464ba6740f5b243ce8a6b49806d713e1aa9693
Atmanand Gauns <sanketgauns8@gmail.com>
2026-05-12 12:03:30 +0530

    release version `1.10.6`

--- src/interfaces/Constants.ts
+++ src/interfaces/Constants.ts
@@ -1,4 +1,4 @@
-export const newReleaseVersion = "1.10.5";
+export const newReleaseVersion = "1.10.6";
 // Plugin view type identifiers
 export const VIEW_TYPE_TASKBOARD = "task-board-view";
 export const VIEW_TYPE_ADD_OR_EDIT_TASK = "add-or-edit-task-view";
